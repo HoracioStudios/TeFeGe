bcd31aae74220ebfca63dc239282b948845d188f
IgnOry <iory@ucm.es>
2021-04-08 15:06:20 +0200

    Menu de seleccion de personajes en ruleta
    Tengo que ver como hago para que roten bonito, lerp de momento es un fracaso

--- Assets/Scripts/Menu/CharacterSelection.cs
+++ Assets/Scripts/Menu/CharacterSelection.cs
@@ -1,39 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterSelection : MonoBehaviour
 {
-    public RectTransform toRotate;
+    public GameObject toRotate;
     int children;
-    float degrees;
+    int degrees;
+    Button left;
+    Button right;
+
+    //Posicion inicial
+    int startAngle = -90;
+    float startPosX = -4.25f;
+    float startPosY = -0.0f;
+    float degreeToRad = Mathf.PI / 180;
+    float radius = 3.25f;
+
+    //Control
+    int characterSelected = 1;
+
+    //Rotacion
+    float speed = 0.1f;
+    float t = 0.0f;
+    bool rotating = false;
 
     private void Start()
     {
+        children = toRotate.transform.childCount;
+        degrees = 360 / children;
 
+        for (int i = 0; i < children; i++)
+        {
+            toRotate.transform.GetChild(i).position = new Vector3(startPosX + radius * Mathf.Cos((startAngle + degrees * i) * degreeToRad), startPosY + radius * Mathf.Sin((startAngle + degrees * i) * degreeToRad), 0);
+        }
+    }
 
-
-        children = toRotate.gameObject.transform.childCount; //Por si se añadieran más personajes en el futuro
-        degrees = 360.0f/children;
+    private void Update()
+    {
+        if (rotating)
+        {
+            if (t < 1.0f)
+                t += Time.deltaTime;
+            else
+            {
+                rotating = false;
+                t = 0.0f;
+                Debug.Log("eee");
+            }
+        }
     }
 
     public void RightArrowClick()
     {
-        toRotate.Rotate(new Vector3(0, 0, degrees));
+        rotating = true;
+
+        Quaternion aux = new Quaternion();
+        aux.eulerAngles = new Vector3(toRotate.transform.rotation.x, toRotate.transform.rotation.y, toRotate.transform.rotation.z - degrees);
+        toRotate.transform.rotation = Quaternion.Slerp(toRotate.transform.rotation, aux, t);
+
+        //.Set(0, 0, Mathf.Lerp(toRotate.transform.rotation.z, toRotate.transform.rotation.z - degrees, Time.deltaTime * speed));
+
+        //toRotate.transform.Rotate(0, 0, -degrees);
 
         for (int i = 0; i < children; i++)
         {
-            toRotate.gameObject.transform.GetChild(i).Rotate(new Vector3(0, 0, -degrees));
+            toRotate.transform.GetChild(i).rotation.eulerAngles.Set(0, 0, Mathf.Lerp(toRotate.transform.GetChild(i).rotation.z, toRotate.transform.GetChild(i).rotation.z + degrees, t * speed));
         }
+
+        if (characterSelected > 0)
+            characterSelected++;
+        else
+            characterSelected = children;
     }
 
     public void LeftArrowClick()
     {
-        toRotate.Rotate(new Vector3(0, 0, -degrees));
+        rotating = true;
+
+        toRotate.transform.rotation.eulerAngles.Set(0, 0, Mathf.Lerp(toRotate.transform.rotation.z, toRotate.transform.rotation.z + degrees, Time.deltaTime * speed));
+
+        //toRotate.transform.Rotate(0, 0, degrees);
 
         for (int i = 0; i < children; i++)
         {
-            toRotate.gameObject.transform.GetChild(i).Rotate(new Vector3(0, 0, degrees));
+            toRotate.transform.GetChild(i).rotation.eulerAngles.Set(0, 0, Mathf.Lerp(toRotate.transform.GetChild(i).rotation.z, toRotate.transform.GetChild(i).rotation.z - degrees, t * speed));
+        }
+
+        if (characterSelected < children)
+            characterSelected++;
+        else
+            characterSelected = 0;
+    }
+
+    public void UpdateCharacterSelected()
+    {
+        switch (characterSelected)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
         }
     }
 }
