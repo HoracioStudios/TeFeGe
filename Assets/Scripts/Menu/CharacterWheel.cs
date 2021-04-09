879ab6c471ee6458b1a12a123d54e4ce28499c61
IgnOry <iory@ucm.es>
2021-04-09 12:34:28 +0200

    Menu de seleccion de personajes encarrilado
    Faltan 2 sprites de habilidades, las descripciones, y conectar con el modo online (para ver que prefab se spawnea)

--- Assets/Scripts/Menu/CharacterWheel.cs
+++ Assets/Scripts/Menu/CharacterWheel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterWheel : MonoBehaviour
 {
@@ -19,9 +20,40 @@ public class CharacterWheel : MonoBehaviour
     public float lerpTime = 0.1f;
     float t = 0.0f;
     public bool rotating = false;
+    private bool dir = false; //False = Right, True = Left
 
     Vector3 startRot;
     Vector3 endRot;
+
+    //Interfaz
+    public Image miniatura;
+    public Sprite miniaturaChuerk;
+    public Sprite miniaturaBadBaby;
+    public Sprite miniaturaCamomila;
+    public Sprite miniaturaManolo;
+    public Sprite miniaturaBob;
+    public Text characterName;
+
+    public Image damageStat;
+    public Sprite damage1;
+    public Sprite damage2;
+    public Sprite damage3;
+
+    public Image miniaturaArma;
+    public Sprite armaChuerk;
+    public Sprite armaBadBaby;
+    public Sprite armaCamomila;
+    public Sprite armaManolo;
+    public Sprite armaBob;
+    public Text descripcionArma;
+
+    public Image miniaturaHabilidad;
+    public Sprite habilidadChuerk;
+    public Sprite habilidadBadBaby;
+    public Sprite habilidadCamomila;
+    public Sprite habilidadManolo;
+    public Sprite habilidadBob;
+    public Text descripcionHabilidad;
 
     private void Start()
     {
@@ -32,6 +64,8 @@ public class CharacterWheel : MonoBehaviour
         {
             transform.GetChild(i).position = transform.position + (new Vector3(radius * Mathf.Cos((startAngle + degrees * i) * degreeToRad), radius * Mathf.Sin((startAngle + degrees * i) * degreeToRad), 0));
         }
+
+        UpdateCharacterSelected();
     }
 
     private void Update()
@@ -55,12 +89,23 @@ public class CharacterWheel : MonoBehaviour
             {
                 rotating = false;
                 t = 0.0f;
-                Debug.Log("eee");
 
-                if (characterSelected > 0)
-                    characterSelected++;
+                if (dir)
+                {
+                    if (characterSelected > 1)
+                        characterSelected--;
+                    else
+                        characterSelected = children;
+                }
                 else
-                    characterSelected = transform.childCount;
+                {
+                    if (characterSelected < children)
+                        characterSelected++;
+                    else
+                        characterSelected = 1;
+                }
+
+                UpdateCharacterSelected();
             }
         }
     }
@@ -68,6 +113,7 @@ public class CharacterWheel : MonoBehaviour
     public void startLerp(bool leftDir)
     {
         rotating = true;
+        dir = leftDir;
 
         Vector3 mod = new Vector3(0, 0, 360.0f / (float)transform.childCount);
 
@@ -82,14 +128,49 @@ public class CharacterWheel : MonoBehaviour
         switch (characterSelected)
         {
             case 1:
+                miniatura.sprite = miniaturaBadBaby;
+                characterName.text = "BAD BABY";
+                miniaturaArma.sprite = armaBadBaby;
+                damageStat.sprite = damage3;
+                descripcionArma.text = "TE MATA CON UN RITMO ENDIABLADO";
+                miniaturaHabilidad.sprite = habilidadBadBaby;
+                descripcionHabilidad.text = "JOSE TRAPERO";
                 break;
             case 2:
+                miniatura.sprite = miniaturaBob;
+                characterName.text = "BOB OJOCOJO";
+                miniaturaArma.sprite = armaBob;
+                damageStat.sprite = damage2;
+                descripcionArma.text = "REBOTA REBOTA Y EN TU CARA EXPLOTA";
+                miniaturaHabilidad.sprite = habilidadBob;
+                descripcionHabilidad.text = "JOSE VA TO CIEGO";
                 break;
             case 3:
+                miniatura.sprite = miniaturaCamomila;
+                characterName.text = "CAMOMILA SESTIMA";
+                miniaturaArma.sprite = armaCamomila;
+                damageStat.sprite = damage3;
+                descripcionArma.text = "PIM PAM TOMA LACASITOS";
+                miniaturaHabilidad.sprite = habilidadCamomila;
+                descripcionHabilidad.text = "A TODO OJETE";
                 break;
             case 4:
+                miniatura.sprite = miniaturaChuerk;
+                characterName.text = "CHUERK CHUERK";
+                miniaturaArma.sprite = armaChuerk;
+                damageStat.sprite = damage1;
+                descripcionArma.text = "SOMEBODY ONCE TOLD ME";
+                miniaturaHabilidad.sprite = habilidadChuerk;
+                descripcionHabilidad.text = "JOSE TIENE CAPAS COMO LAS CEBOLLAS";
                 break;
             case 5:
+                miniatura.sprite = miniaturaManolo;
+                characterName.text = "MANOLO MCFLY";
+                miniaturaArma.sprite = armaManolo;
+                damageStat.sprite = damage2;
+                descripcionArma.text = "RAFAGAS COMO EN LOS COCHES DE CHOQUE";
+                miniaturaHabilidad.sprite = habilidadManolo;
+                descripcionHabilidad.text = "JOSE MANOLO";
                 break;
         }
     }
