b55b1476082a25e90505774bcb4b3a678897d3c0
javi <javari02@ucm.es>
2021-04-11 17:57:13 +0200

    Se arregló el bug de que solo se moría un jugador en el cliente opuesto

--- Assets/Scripts/Attacks/damage.cs
+++ Assets/Scripts/Attacks/damage.cs
@@ -12,7 +12,8 @@ public class damage : MonoBehaviour
     //Only do damage when collides with other team pj
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("BTeam")) return;
+        //if (collision.gameObject.layer == LayerMask.NameToLayer("BTeam")) return;
+
         //Ignore collisions with other bullets and handle collisions with "enemies"
         if ("Wall" != collision.gameObject.tag)
         {
