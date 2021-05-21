065b938075f9a3f2a897dda1ef355999e7b4150a
PabloGarciaGrossi <pabgar20@ucm.es>
2021-05-21 12:01:19 +0200

    Comienzo Lobby

--- Assets/Scripts/Menu/GoToLobby.cs
+++ Assets/Scripts/Menu/GoToLobby.cs
@@ -2,36 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToLobby : MonoBehaviour
 {
-    float time = 0;
-    private void Update()
-    {
-        try
-        {
-            Rival found = ClientCommunication.SearchPair(GameManager.instance.player.playerID, time);
-            if (!found.found)
-            {
-                time += Time.deltaTime;
-            }
-            else
-            {
-                //Ir a partida
-                if (Mirror.NetworkManager.singleton)
-                    Mirror.NetworkManager.singleton.StartClient();
-            }
-        } catch (RestResponseException e)
-        {
-            Debug.Log("Error en búsqueda de pareja: " + e.Message);
-        }
-        //Llamar a el findPair del servidor de Matchmaking
-        
-    }
     public void Go()
     {
-        //ESTO HAY QUE CAMBIARLO CUANDO HAGAMOS EL LOBBY
-        if(Mirror.NetworkManager.singleton)
-            Mirror.NetworkManager.singleton.StartClient();
+        SceneManager.LoadSceneAsync("WaitingLobby");
     }
 }
---
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public Text waitText;
+    float textTime = 0f;
+    public GameObject sprite;
+    float time = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        canvasUpdate();
+
+       // searchPlayer();
+    }
+
+    void canvasUpdate()
+    {
+        if (textTime < 3.75)
+        {
+            textTime += Time.deltaTime;
+            if (textTime >= 0.75f && textTime < 1.5f)
+            {
+                waitText.text = "WAITING FOR PLAYER.";
+            }
+            else if (textTime >= 1.5f && textTime < 2.25f)
+            {
+                waitText.text = "WAITING FOR PLAYER..";
+            }
+            else if (textTime >= 2.25f && textTime < 3f)
+            {
+                waitText.text = "WAITING FOR PLAYER...";
+            }
+            else { waitText.text = "WAITING FOR PLAYER"; }
+        }
+        else { textTime = 0f; }
+
+        sprite.transform.Rotate(Vector3.forward * -50f * Time.deltaTime);
+    }
+
+    void searchPlayer()
+    {
+        try
+        {
+            Rival found = ClientCommunication.SearchPair(GameManager.instance.player.playerID, time);
+            if (!found.found)
+            {
+                time += Time.deltaTime;
+            }
+            else
+            {
+                //Ir a partida
+                if (Mirror.NetworkManager.singleton)
+                    Mirror.NetworkManager.singleton.StartClient();
+            }
+        }
+        catch (RestResponseException e)
+        {
+            Debug.Log("Error en búsqueda de pareja: " + e.Message);
+        }
+        //Llamar a el findPair del servidor de Matchmaking
+    }
+}
